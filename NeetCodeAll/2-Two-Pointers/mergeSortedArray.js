2966ecec1374db9baba3d298959f543c150b77be
Trenton Bensheimer <tbensheimer@gmail.com>
2023-10-11 23:04:54 -0400

    Neetcode mergeSortedArray

---
+++ NeetCodeAll/2-Two-Pointers/mergeSortedArray.js
@@ -0,0 +1,28 @@
+let merge = (nums1, m, nums2, n) => {
+    let i = m  - 1;
+    let j = n -  1;
+    let k = m + n - 1;
+
+    while(i >= 0 &&  j >= 0) {
+        if(nums1[i] < nums2[j]) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        } else {
+            nums1[k] = nums1[i];
+            i--;
+            k--;
+        }
+    }
+
+    while(j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+    }
+}
+
+console.log(merge([1,2,3,0,0,0], 3, [2,5,6], 3)) //[1,2,2,3,5,6]
+
+//time - O(N)
+//space - O(1)
